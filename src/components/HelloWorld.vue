eb323f54bf02c75b2be6e43f49ede79bf1d259de
Your Name <you@example.com>
2018-01-22 16:26:24 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="box">
+      <img :src="require('../assets/1.png')"/>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped lang="scss">
+  .box{
+    img{
+      width:100px;
+      height:100px;
+    }
+  }
+</style>
+
+
